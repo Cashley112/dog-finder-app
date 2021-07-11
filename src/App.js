d8d9a748925a3c77eebb9c81e93da69287a954c0
Connor Ashley <ashleycm@vcu.edu>
2021-07-11 19:08:35 -0400

    basic route paths for DogList and Dog components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import {Route, Switch, NavLink} from 'react-router-dom';
 import DogList from './DogList';
+import Dog from './Dog';
 
 function App() {
   return (
     <div className="App">
-      <DogList />
+      <Switch>
+        <Route 
+          exact path='/'
+          component={DogList}
+        />
+        <Route 
+          exact path='/dogs/:name'
+          component={Dog}
+        />
+      </Switch>
     </div>
   );
 }
