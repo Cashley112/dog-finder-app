c7b0465885e7fcebebc22aacf46eefd033c48e3e
Connor Ashley <ashleycm@vcu.edu>
2021-07-11 19:09:15 -0400

    most basic version of components, no state or props

---
+++ src/DogList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './DogList.css';
+
+class DogList extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Dogs In Need of Love</h1>
+            </div>
+        )
+    }
+}
+
+export default DogList;
