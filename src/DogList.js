9cfc4f09f6de377a6039fc3aba81f151890381fe
Connor Ashley <ashleycm@vcu.edu>
2021-07-12 13:24:21 -0400

    inherited dog data from parent component, map over array to display a responsive div including some info about each element

--- src/DogList.js
+++ src/DogList.js
@@ -4,8 +4,18 @@ import './DogList.css';
 class DogList extends Component {
     render() {
         return(
-            <div>
-                <h1>Dogs In Need of Love</h1>
+            <div className="DogList">
+                <h1 className="display-1 text-center">Dogs In Need of Love</h1>
+                <div className="container">
+                    <div className="row">
+                        {this.props.dogs.map(d => (
+                            <div className='Dog col-lg-4 text-center' key={d.name}>
+                                <img src={d.src} alt={d.name} />
+                                <h3>{d.name}</h3>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         )
     }
