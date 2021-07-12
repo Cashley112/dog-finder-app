97f0cb0a5227da163841b4f6b544d18adc66e7f5
Connor Ashley <ashleycm@vcu.edu>
2021-07-12 13:23:02 -0400

    included and imported dog images, as well as dogs starter data

--- src/App.js
+++ src/App.js
@@ -3,48 +3,54 @@ import {Route, Switch, NavLink} from 'react-router-dom';
 import DogList from './DogList';
 import Dog from './Dog';
 import { render } from 'react-dom';
+import charlie from './dogImages/charlie.jpg';
+import lumpy from './dogImages/lumpy.jpg';
+import tiddwell from './dogImages/tiddwell.jpg';
 import React, { Component } from 'react';
 
 
 class App extends Component {
+  static defaultProps = {
+    dogs: [
+      {
+        name: "Charlie",
+        age: 5,
+        src: charlie,
+        facts: [
+          "Whiskey loves eating popcorn.",
+          "Whiskey is a terrible guard dog.",
+          "Whiskey wants to cuddle with you!"
+        ]
+      },
+      {
+        name: "Lumpy",
+        age: 3,
+        src: lumpy,
+        facts: [
+          "Hazel has soooo much energy!",
+          "Hazel is highly intelligent.",
+          "Hazel loves people more than dogs."
+        ]
+      },
+      {
+        name: "Tiddwell",
+        age: 4,
+        src: tiddwell,
+        facts: [
+          "Tubby is not the brightest dog",
+          "Tubby does not like walks or exercise.",
+          "Tubby loves eating food."
+        ]
+      }
+    ]
+  }
   render() {
     return(
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider"></hr></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-              </li>
-            </ul>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
+      <div>
+        <Switch>
+          <Route to='/dogs' render={() => <DogList dogs={this.props.dogs} />} />
+        </Switch>
+      </div>
     )
   }
 }
